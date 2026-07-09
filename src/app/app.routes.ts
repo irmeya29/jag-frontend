d0363b0afca69a5c7770ed881ddbf44931d88773
romafoxz <bationor77@gmail.com>
2026-07-09 15:08:57 +0000

    Mise en place des composants UI globaux et du footer et navbar réutilisables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { Apropos } from './pages/apropos/apropos';
 import { Monespacejust } from './pages/monespacejust/monespacejust';
 import { Notreprocessus } from './pages/notreprocessus/notreprocessus';
 import { Realisations } from './pages/realisations/realisations';
+import { Pole1 } from './nospoles/pole1/pole1';
+import { Pole2 } from './nospoles/pole2/pole2';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
@@ -16,6 +18,8 @@ export const routes: Routes = [
   { path: 'mon-espace-just', component: Monespacejust },
   { path: 'notre-processus', component: Notreprocessus },
   { path: 'realisations', component: Realisations },
+  { path: 'nospoles/pole1', component: Pole1 },
+  { path: 'nospoles/pole2', component: Pole2 },
 
   // Redirection par défaut
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  isMenuOpen = signal(false);
+  isDropdownOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
+
+  toggleDropdown(event: Event) {
+    event.stopPropagation();
+    this.isDropdownOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+    this.isDropdownOpen.set(false);
+  }
+}
